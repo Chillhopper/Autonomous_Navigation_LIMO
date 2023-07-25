9a633c1a2bb1c39d4ee3c699630318200c8a869b
Chillhopper <68851163+Chillhopper@users.noreply.github.com>
2023-07-25 14:31:11 +0800

    More comments

--- pos.py
+++ pos.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 
+"""
+@file       egui.py
+@author     Team 3 (Muhammad Fazrey Bin Zainal, Kaushik Thirumavalavan, Akmal Rusyaidi Bin Muhammad Tajwid,
+            Muhammad Zulhilman Bin Mohd Shaini, Chang Keng Jethro, Janaishwaran)
+@course     Robotics Systems Engineering, Singapore Institute of Technology
+@module     SEP
+@date       24/07/2023
+@brief      This script retrieves information from the /amcl_pose and /odom topic and converts the position 
+            and orientation information into a usable array format that can be directly use in the coordinate base
+"""
+
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from nav_msgs.msg import Odometry
@@ -7,18 +18,18 @@ val = 0
 
 # Node initialization
 rospy.init_node('init_pose')
-pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size = 1)
 
+# Function to extract x, y, z, and w values from a given text
 def extractor(text):
     values = []
     lines = text.strip().split('\n')
 
     for line in lines:
         if ':' in line:
-            key, value = line.split(':')
-            key = key.strip()
-            value = value.strip()
-            if key in ['x', 'y', 'z', 'w']:
+            key, value = line.split(':') #finds messages in the text with a colon and converts them into key and value pair
+            key = key.strip() #cleans the string by removing white space
+            value = value.strip() #cleans the string by removing white space
+            if key in ['x', 'y', 'z', 'w']: #checks if the line contains coordinate information
                 if value:
                     try:
                         value = float(value)
@@ -29,34 +40,29 @@ def extractor(text):
     print(values)
 
 def get_odom():
-  print("ODOM------------------------------------------------------/")
-  # Construct message
-  init_msg = PoseWithCovarianceStamped()
-  init_msg.header.frame_id = "map"
-  # Get initial pose from Gazebo
+    # Get Odometry pose from Gazebo
   odom_msg = rospy.wait_for_message('/odom', Odometry)
-  init_msg.pose.pose.position.x = odom_msg.pose.pose.position.x
-  init_msg.pose.pose.position.y = odom_msg.pose.pose.position.y
-  init_msg.pose.pose.orientation.x = odom_msg.pose.pose.orientation.x
-  init_msg.pose.pose.orientation.y = odom_msg.pose.pose.orientation.y
-  init_msg.pose.pose.orientation.z = odom_msg.pose.pose.orientation.z
-  init_msg.pose.pose.orientation.w = odom_msg.pose.pose.orientation.w
 
-  extractor(str(init_msg))
-  return init_msg
+  print("ODOM------------------------------------------------------/")
+  # Print the receives message in array format
+  extractor(str(odom_msg))
+
+  # Return the pose message
+  return odom_msg
 
 
 def get_amcl():
+    # Get amcl pose from Gazebo
   amcl_pose_msg = rospy.wait_for_message('/amcl_pose', PoseWithCovarianceStamped)
   
   print("AMCL------------------------------------------------------/")
-  # Print the received pose message
+  # Print the received pose message in array format
   extractor(str(amcl_pose_msg))
 
   # Return the pose message
   return amcl_pose_msg    
   
-
+# Keeps looping for input, gets amcl coords if 3 is input, odom coords if 2 is input
 while True:
   val = input("enter 2 for /odom and 3 for /amcl_pose for coords: ")
   if val == 2:
--- poseHub.py
+++ poseHub.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 
+"""
+@file       poseHub.py
+@author     Team 3 (Muhammad Fazrey Bin Zainal, Kaushik Thirumavalavan, Akmal Rusyaidi Bin Muhammad Tajwid,
+            Muhammad Zulhilman Bin Mohd Shaini, Chang Keng Jethro, Janaishwaran)
+@course     Robotics Systems Engineering, Singapore Institute of Technology
+@module     SEP
+@date       24/07/2023
+@brief      This script allows the user to control the pose estimate of the robot through the terimnal using WASDJK controls
+
+"""
+
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped
 import threading
@@ -10,56 +21,84 @@ import sys, select, termios, tty
 wasd = 0.02
 jk = 0.02
 
+# Initialize the ROS node with the name 'pose_publisher'
 rospy.init_node('pose_publisher')
+
+# Create a publisher to publish the initial pose on the topic '/initialpose'
 pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size=10)
+
+# Global variable to store the current pose
 curr_pose = None
 
+# Callback function to update the current pose when new data is received on '/amcl_pose' topic	
 def pose_callback(pose_msg):
-	global curr_pose
-	curr_pose = pose_msg
-	
+    global curr_pose
+    curr_pose = pose_msg
+
+# Function to read a single key from the keyboard without the need to press enter to register input
+# Function to read a single key from the keyboard without the need to press enter to register input
 def getKey(key_timeout):
+    # Get the current terminal settings to be restored later
     settings = termios.tcgetattr(sys.stdin)
+    
+    # Set the terminal to raw mode, which allows reading a single character at a time without buffering
     tty.setraw(sys.stdin.fileno())
+
+    # Use the select module to check for keyboard input with a specified timeout
+    # rlist will contain the file objects that are ready to be read
     rlist, _, _ = select.select([sys.stdin], [], [], key_timeout)
+
+    # Check if there is any keyboard input available within the specified timeout
     if rlist:
+        # Read a single character from the standard input (stdin)
         key = sys.stdin.read(1)
     else:
+        # If there is no input available within the timeout, set the key to an empty string
         key = ''
+
+    # Restore the original terminal settings to allow normal terminal behavior
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
+    # Return the single character read from the keyboard or an empty string if no input is available
     return key
 
+# Subscriber function to subscribe to the '/amcl_pose' topic and update the current pose
 def subscriber():
-	rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, pose_callback)	
+    rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, pose_callback)
 
-
-print("position: wasd, orientation: jk,  'p' to exit")
+# Main loop to continuously update the pose and publish it when a key is pressed
+print("position: wasd, orientation: jk, 'p' to exit")
 while True:
-	subscriber() #updates current position
-	if curr_pose is not None:
-		pose_msg = curr_pose
-		key_timeout = rospy.get_param("~key_timeout", 0.0)
-		val = raw_input("wasd: ")
-		if val == 'w':
-			print('w\n')
-			pose_msg.pose.pose.position.x+=wasd
-		elif val == 'a':
-			print('a\n')
-			pose_msg.pose.pose.position.y+=wasd
-		elif val == 's':
-			print('s\n')
-			pose_msg.pose.pose.position.x-=wasd
-		elif val == 'd':
-			print('d\n')
-			pose_msg.pose.pose.position.y-=wasd
-                elif val == 'j':
-			print('j\n')
-			pose_msg.pose.pose.orientation.z+=jk
-                elif val == 'k':
-			print('k\n')
-			pose_msg.pose.pose.orientation.z-=jk
-		elif val == 'p':
-		     print("exiting...")
-		     break
-		
-		pub.publish(pose_msg)	
+    subscriber()  # Update current position
+    if curr_pose is not None:
+        pose_msg = curr_pose
+        key_timeout = rospy.get_param("~key_timeout", 0.0)
+        val = getKey(key_timeout)
+        wasd = 0.1  # Translation increment value
+        jk = 0.1    # Orientation increment value
+
+        # Check which key is pressed and modify the pose accordingly
+        if val == 'w':
+            print('w\n')
+            pose_msg.pose.pose.position.x += wasd  # Increment x position
+        elif val == 'a':
+            print('a\n')
+            pose_msg.pose.pose.position.y += wasd  # Increment y position
+        elif val == 's':
+            print('s\n')
+            pose_msg.pose.pose.position.x -= wasd  # Decrement x position
+        elif val == 'd':
+            print('d\n')
+            pose_msg.pose.pose.position.y -= wasd  # Decrement y position
+        elif val == 'j':
+            print('j\n')
+            pose_msg.pose.pose.orientation.z += jk  # Increment orientation (z-axis)
+        elif val == 'k':
+            print('k\n')
+            pose_msg.pose.pose.orientation.z -= jk  # Decrement orientation (z-axis)
+        elif val == 'p':
+            print("exiting...")  # Print message when 'p' key is pressed
+            break  # Exit the loop and end the program
+
+        # Publish the modified pose
+        pub.publish(pose_msg)
